6e53a211d454d4d972707d09f02a71c1d2af57af
SAI-13100 <saiprasath4669@gmail.com>
2025-03-26 06:44:29 +0530

    Update main.rs

--- seanime-desktop/src-tauri/src/main.rs
+++ seanime-desktop/src-tauri/src/main.rs
@@ -1,12 +1,22 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 fn main() {
     #[cfg(target_os = "linux")]
     {
         std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
     }
 
-
-    app_lib::run();
+    tauri::Builder::default()
+        .setup(|app| {
+            let app_handle = app.handle();
+            app.listen("tauri://close-requested", move |_| {
+                app_handle.exit(0); // Fully quits the app
+            });
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running Tauri application");
 }
